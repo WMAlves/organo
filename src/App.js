571ba51f0796ac1514c4418178bb0877d4ab33f9
WMAlves <williamma03@gmail.com>
2023-04-26 10:59:18 -0300

    Cirado o projeto React, criado componetes funcionas, Banner, CampoTexto, Formulário, foi estilizado os componetes com CSS

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import Banner from './componetes/Banner';
+import Formulario from './componetes/Fomulario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario/>
+      
+    </div>
+  );
+}
+
+export default App;
