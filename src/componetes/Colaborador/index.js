6eea8f5342db0c72cb3f26b80e92b249e820bb6d
William M. Alves <williamma03@gmail.com>
2023-07-30 23:22:23 -0300

    foi adicionado o compomente colaborador e alterado o componente time

---
+++ src/componetes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador =()=>{
+    return(
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/wmalves.png' alt='William M. Alves'/>
+            </div>
+            <div className='rodape'>
+                <h4>William M.Alves</h4>
+                <h5>Desenvolvedor Frontend</h5>
+
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
