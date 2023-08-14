8d5c5275378db93ea24c65984c2e6443761ffdb7
William M. Alves <williamma03@gmail.com>
2023-08-14 19:43:43 -0300

    feita a finalização do projeto em sua primeira parte

--- src/App.js
+++ src/App.js
@@ -3,70 +3,74 @@ import Banner from "./componetes/Banner";
 import Formulario from "./componetes/Fomulario";
 import Time from "./componetes/Time";
 
-
 function App() {
   const times = [
     /*cada estrutura abaixo é um time que será definido como time na função map
     abaixo*/
     {
-      nome:'Programação',
-      corPrimaria:'#57C278',
-      corSecundaria:'#D9F7E9'
+      nome: "Programação",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
     },
     {
-      nome:'Front-End',
-      corPrimaria:'#82CFFA',
-      corSecundaria:'#E8F8FF'
+      nome: "Front-End",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
     },
     {
-      nome:'Data Science',
-      corPrimaria:'#A6D157',
-      corSecundaria:'#F0F8E2'
+      nome: "Data Science",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
     },
     {
-      nome:'Devops',
-      corPrimaria:'#E06B69',
-      corSecundaria:'#FDE7E8'
+      nome: "Devops",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
     },
     {
-      nome:'UX e Design',
-      corPrimaria:'#DB6EBF',
-      corSecundaria:'#FAE9F5'
+      nome: "UX e Design",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
     },
     {
-      nome:'Mobile',
-      corPrimaria:'#FFBA05',
-      corSecundaria:'#FFF5D9'
+      nome: "Mobile",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
     },
     {
-      nome:'Inovação e Gestão',
-      corPrimaria:'#FF8A29',
-      corSecundaria:'#FFEEDF'
+      nome: "Inovação e Gestão",
+      corPrimaria: "#FF8A29",
+      corSecundaria: "#FFEEDF",
     },
-  ]
+  ];
 
+  const [colaboradores, setColaboradores] = useState([]);
 
-  const [colaboradores, setColaboradores]= useState([])
-
-  const aoNovoColaboradorAdicionado = (colaborador)=>{
-    console.log(colaborador)
-    setColaboradores([...colaboradores,colaborador])
-
-  }
+  const aoNovoColaboradorAdicionado = (colaborador) => {    
+    setColaboradores([...colaboradores, colaborador]);
+  };
 
   return (
-
     <div className="App">
-      <Banner/>    
-      <Formulario times={times.map(time=>time.nome)} aoColaboradorCadastrado={colaborador=>aoNovoColaboradorAdicionado(colaborador)}/>      
+      <Banner />
+      <Formulario
+        times={times.map((time) => time.nome)}
+        aoColaboradorCadastrado={(colaborador) =>
+          aoNovoColaboradorAdicionado(colaborador)
+        }
+      />
 
-      {times.map(time=> <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}
-        colaboradores={colaboradores.filter(colaborador=> colaborador.time === time.nome)}
-        />)}
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+          colaboradores={colaboradores.filter(
+            (colaborador) => colaborador.time === time.nome
+          )}
+        />
+      ))}
     </div>
     /*acima foi criado a função map do javascript, onde peguei o array times e com a função '.map'
       atribui como ele deveria se comportar. Nesse caso será devolvido atraves do '.map' o nome do time
@@ -75,6 +79,6 @@ function App() {
 
     */
   );
-} 
- 
+}
+
 export default App;
